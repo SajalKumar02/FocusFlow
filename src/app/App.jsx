c9a2ed7da4693ea0dcdb34b5b62c9eeaa306ca3d
Sajal Kumar <learning4sajal@gmail.com>
2026-03-02 19:16:29 +0530

    Remove TodoList, TodoFilterSortRow, context, and reducer files; simplify App component structure.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,21 +1,7 @@
-import TodoList from "../features/components/TodoList.jsx";
-import AddTodoButton from "../features/components/AddTodoButton.jsx";
-import TodoFilterSortRow from "../features/components/TodoFilterSortRow.jsx";
+import React from "react";
 
 const App = () => {
-  return (
-    <div className="h-screen bg-amber-50 flex flex-col items-center justify-center">
-      <h1 className="text-3xl font-mono font-bold mb-4">Todo List</h1>
-      {/* Filter Sort Row */}
-      <TodoFilterSortRow />
-
-      {/* List */}
-      <TodoList />
-
-      {/* Add Todo Button */}
-      <AddTodoButton />
-    </div>
-  );
+  return <div>App</div>;
 };
 
 export default App;
