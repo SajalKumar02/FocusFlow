40b4f6c6ca9ec5d96168c82d9e785df96c804d56
SajalKumar02 <work.sajalkumar@gmail.com>
2026-06-29 00:48:55 +0530

    Update import statement for Settings component to include file extension for consistency with other imports.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -7,7 +7,7 @@ import Layout from '@/layout/Layout';
 import { MainContent } from '@/feature/task';
 
 import TaskDetails from '@/pages/TaskDetails';
-import Settings from '@/pages/Settings';
+import Settings from '@/pages/Settings.jsx';
 
 const App = () => {
   return (
